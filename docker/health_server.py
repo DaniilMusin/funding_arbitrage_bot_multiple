4b4ce1cf10d293876ede0baa643f2fe14cb69d24
alekseydmsoloviev <alekseydmsoloviev@gmail.com>
2025-07-08 14:56:19 +0300

    Add health server and extended Dockerfile

---
+++ docker/health_server.py
@@ -0,0 +1,23 @@
+import os
+import time
+import psutil
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/livez')
+def livez():
+    return 'alive', 200
+
+@app.route('/metrics')
+def metrics():
+    data = {
+        'cpu_percent': psutil.cpu_percent(interval=0.1),
+        'memory_percent': psutil.virtual_memory().percent,
+        'timestamp': time.time(),
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    port = int(os.getenv('HEALTH_PORT', '8080'))
+    app.run(host='0.0.0.0', port=port)
